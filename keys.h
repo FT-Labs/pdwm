7fa75f3ec2eca1c963fad20b7d7eea9532a67ed4
Arda Atci <phystecharda@gmail.com>
2022-07-16 01:53:29 +0300

    welcome app removed from cfg

--- keys.h
+++ keys.h
@@ -40,7 +40,6 @@ static const Rule rules[] = {
     { NULL,      "key_pdf",              NULL,            0,            1,           1,         1,         0,        -1 },
     { NULL,       NULL,                  "pulsemixer",    0,            1,           1,         1,         1,        -1 },
     { NULL,       NULL,                  "nmtui",         0,            1,           1,         1,         1,        -1 },
-    { NULL,      "phyOS-welcome-app",    NULL,            0,            0,           1,         1,         1,        -1 },
 };
 
 static Key keys[] = {
