e798ad2bb345d04c20c63e5704a920da8ab7cbea
Arda Atci <phystecharda@gmail.com>
2022-07-20 01:45:21 +0300

    script chg

--- dwmblocks.h
+++ dwmblocks.h
@@ -6,9 +6,9 @@
 ALLOC_BLOCKS /* DON'T DELETE THIS LINE */
                     /*Command*/     /*Update Interval*/ /*Update Signal*/
 blocks[0] = (Block) {"cat /tmp/recordingicon 2>/dev/null",  0,  9};
-blocks[1] = (Block) {"crypto",  360,    6};
-blocks[2] = (Block) {"weather", 18000,  5};
-blocks[3] = (Block) {"volume",  0,  10};
-blocks[4] = (Block) {"battery", 5,  3};
-blocks[5] = (Block) {"clock",   60, 1};
-blocks[6] = (Block) {"internet",    5,  4};
+blocks[1] = (Block) {"pOS-crypto",  360,    6};
+blocks[2] = (Block) {"pOS-weather", 18000,  5};
+blocks[3] = (Block) {"pOS-volume",  0,  10};
+blocks[4] = (Block) {"pOS-battery", 5,  3};
+blocks[5] = (Block) {"pOS-clock",   60, 1};
+blocks[6] = (Block) {"pOS-internet",    5,  4};
